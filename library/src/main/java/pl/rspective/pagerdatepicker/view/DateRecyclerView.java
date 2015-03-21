24eec3b03c1e1813de35dc0663b39957c105d8c2
marcin.polak <mpolak87@gmail.com>
2015-03-21 09:55:00 +0100

    First version supports only LinearLayoutManager and horizontal scrolling

--- library/src/main/java/pl/rspective/pagerdatepicker/view/DateRecyclerView.java
+++ library/src/main/java/pl/rspective/pagerdatepicker/view/DateRecyclerView.java
@@ -1,6 +1,7 @@
 package pl.rspective.pagerdatepicker.view;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 
@@ -16,5 +17,19 @@ public class DateRecyclerView extends RecyclerView {
 
     public DateRecyclerView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setLayoutManager(LayoutManager layout) {
+        if(!(layout instanceof LinearLayoutManager)) {
+            throw new IllegalArgumentException("For now DateRecyclerView supports only LinearLayoutManager");
+        }
+
+        LinearLayoutManager linearLayout = (LinearLayoutManager) layout;
+        if(linearLayout.getOrientation() != LinearLayoutManager.HORIZONTAL) {
+            throw new IllegalArgumentException("For now DateRecyclerView supports only horizontal scrolling");
+        }
+
+        super.setLayoutManager(layout);
     }
 }
