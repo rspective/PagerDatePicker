2d43a14fc318719f6673012935739dac77ed9587
marcin.polak <mpolak87@gmail.com>
2015-03-24 20:31:11 +0100

    Changed insets value name

--- library/src/main/java/pl/rspective/pagerdatepicker/view/RecyclerViewInsetDecoration.java
+++ library/src/main/java/pl/rspective/pagerdatepicker/view/RecyclerViewInsetDecoration.java
@@ -25,14 +25,14 @@ import pl.rspective.pagerdatepicker.R;
 
 public class RecyclerViewInsetDecoration extends RecyclerView.ItemDecoration {
 
-    private int mInsets;
+    private int insets;
 
     public RecyclerViewInsetDecoration(Context context) {
-        mInsets = context.getResources().getDimensionPixelSize(R.dimen.date_card_insets);
+        insets = context.getResources().getDimensionPixelSize(R.dimen.date_card_insets);
     }
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        outRect.set(mInsets, mInsets, mInsets, mInsets);
+        outRect.set(insets, insets, insets, insets);
     }
 }
